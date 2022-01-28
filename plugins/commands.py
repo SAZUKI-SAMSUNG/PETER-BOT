59b3d098efd199eb5d894e8ff04b91a332ac8ff3
SAZUKI-SAMSUNG <98511431+SAZUKI-SAMSUNG@users.noreply.github.com>
2022-01-28 22:14:54 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -2,7 +2,6 @@ import os
 import logging
 import random
 import asyncio
-from Script import script
 from pyrogram import Client, filters
 from pyrogram.errors.exceptions.bad_request_400 import ChatAdminRequired
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
