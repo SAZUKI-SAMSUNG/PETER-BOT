60db0e7a74f87f925df85cce0332a719ed2d73b8
SAZUKI-SAMSUNG <98511431+SAZUKI-SAMSUNG@users.noreply.github.com>
2022-01-29 12:40:05 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -91,9 +91,9 @@ async def answer(bot, query):
 def get_reply_markup(query):
     buttons = [
         [
-            InlineKeyboardButton('♻️ 𝗦𝗲𝗮𝗿𝗰𝗵 𝗔𝗴𝗮𝗶𝗻 ♻️', switch_inline_query_current_chat=query)
+            InlineKeyboardButton('𝙸𝙽𝙻𝙸𝙽𝙴', switch_inline_query_current_chat=query)
         ],[
-            InlineKeyboardButton('♥️ 𝗖𝗵𝗮𝗻𝗻𝗲𝗹 ♥️', url='https://t.me/+veUIdIW2CQ5mOGU5')
+            InlineKeyboardButton('𝙾𝚆𝙽𝙴𝚁', url='https://t.me/TEAM_KERALA')
         ]
         ]
     return InlineKeyboardMarkup(buttons)
