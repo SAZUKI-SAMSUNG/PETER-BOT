8b9e46b6312c387df1d42463183bdb1d5ffbd197
SAZUKI-SAMSUNG <98511431+SAZUKI-SAMSUNG@users.noreply.github.com>
2022-01-31 13:59:25 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -38,10 +38,10 @@ async def start(client, message):
     if len(message.command) != 2:
         buttons = [[
             InlineKeyboardButton(' 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝙰 𝙲𝙷𝙰𝚃 𝙶𝚁𝚄𝙾𝙿 ', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
-            InlinekeyboardButton(' 𝙳𝙴𝚅𝙴𝙻𝙾𝙿𝙴𝚁 ', url='https://t.me/TEAM_KERALA')
+            InlinekeyboardButton('𝙾𝚆𝙽𝙴𝚁', url='https://t.me/TEAM_KERALA'),
             ],[
             InlineKeyboardButton(' 𝙶𝚁𝙾𝚄𝙿 ', url='https://t.me/+02irdICknwJkYzc5'),
-            InlineKeyboardButton(' 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 ', url='https://t.me/TEAMSAZUKI')
+            InlineKeyboardButton(' 𝙳𝙴𝚅𝙴𝙻𝙾𝙿𝙴𝚁 ', url='https://t.me/TEAM_KERALA'),
             ],[
             InlineKeyboardButton(' 𝙷𝙴𝙻𝙿 ', callback_data='help'),
             InlineKeyboardButton(' 𝙰𝙱𝙾𝚄𝚃', callback_data='about')
